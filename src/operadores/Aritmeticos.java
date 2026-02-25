aea7670096dfb772bcc35f940077fc43d29afed7
Andriw Gabryel <agfmondego5@gmail.com>
2026-02-25 14:21:03 -0300

    Aula de Operadores Aritméticos.

---
+++ src/operadores/Aritmeticos.java
@@ -0,0 +1,28 @@
+package operadores;
+
+public class Aritmeticos {
+    public static void main(String[] args) {
+        int a = 36;
+        int b = 5;
+
+        // adição
+        int adição = a + b;
+        System.out.println("adição: " + adição);
+
+        // subtração
+        int subtração = a - b;
+        System.out.println("subtração: " + subtração);
+
+        // multiplicação
+        int multiplicação = a * b;
+        System.out.println("Multiplicação: " + multiplicação);
+
+        // Divisão
+        int Divisão = a / b;
+        System.out.println("Divisão: " + Divisão);
+
+        // Resto (dá pra saber se é par ou impar)
+        int Resto = a % b;
+        System.out.println("Resto: " + Resto);
+    }
+}
